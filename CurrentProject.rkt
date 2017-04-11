39f13732192628260f74f5299719d99cf3825ceb
zaheelee <zaheelee@gmail.com>
2017-04-11 18:33:31 -0400

    added a new file for helper functions

--- CurrentProject.rkt
+++ CurrentProject.rkt
@@ -6,6 +6,7 @@
 (load "functionParser.scm")
 
 (require racket/trace)
+(include "HelperFunctions.rkt")
 
 ;Takes a file from the local directory and, using simpleParser, generates a parse tree (in list format) of the text in the file. The parse tree modifies the text of the original document
 ;such that it is made suitable for scheme (for example, if one line was "x + y;", then simpleParser would generate said expression as (+ x y).
@@ -529,64 +530,3 @@
                    (evaluate_function (first rest_of_program) (rest rest_of_program) boxed_state function_return))))))
     
 
-;This section is for abstractions of the car/cdr functions. first and rest are already implemented by Pretty Big
-
-;Functionality of caar
-(define first_of_first
-  (lambda (l)
-    (caar l)))
-
-;Functionality of cddr
-(define rest_of_rest
-  (lambda (l)
-    (cddr l)))
-
-;Functionality of cadr
-(define first_of_rest
-  (lambda (l)
-    (cadr l)))
-
-;Functionality of cdar
-(define rest_of_first
-  (lambda (l)
-    (cdar l)))
-
-;Functionality of caaar
-(define first_of_first_of_first
-  (lambda (l)
-    (caaar l)))
-
-;Functionality of caadr
-(define first_of_first_of_rest
-  (lambda (l)
-    (caadr l)))
-
-;Functionality of cadar
-(define first_of_rest_of_first
-  (lambda (l)
-    (cadar l)))
-
-;Functionality of caddr
-(define first_of_rest_of_rest
-  (lambda (l)
-    (caddr l)))
-
-;Functionality of cdaar
-(define rest_of_first_of_first
-  (lambda (l)
-    (cdaar l)))
-
-;Functionality of cdadr
-(define rest_of_first_of_rest
-  (lambda (l)
-    (cdadr l)))
-
-;Functionality of cddar
-(define rest_of_rest_of_first
-  (lambda (l)
-    (cddar l)))
-
-;Functionality of cdddr
-(define rest_of_rest_of_rest
-  (lambda (l)
-    (cdddr l)))
